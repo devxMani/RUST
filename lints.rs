ab3a0b94525adca56d1b4eb092e004e46c560ac5
MANI <chandramanigupta2003@gmail.com>
2024-09-29 11:58:58 +0530

    first commit

---
+++ lints.rs
@@ -0,0 +1,16 @@
+$ cat main.rs
+fn main() {
+    let x = 5;
+}
+$ rustc main.rs
+warning: unused variable: `x`
+ --> main.rs:2:9
+  |
+2 |     let x = 5;
+  |         ^
+  |
+  = note: `#[warn(unused_variables)]` on by default
+  = note: to avoid this warning, consider using `_x` instead
+
+warning: unused variable: `x`
+ --> main.rs:2:9
